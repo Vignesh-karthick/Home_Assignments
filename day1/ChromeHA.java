0a74425205be2231884047463876304972ba76b6
Vignesh-karthick <vignesh211997@gmail.com>
2024-06-23 19:39:58 +0530

    Add files via upload
    Day1 classroom assignments and home assignments.

---
+++ day1/ChromeHA.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class ChromeHA {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("The browser version is "+version);
+		System.out.println("Developer is " +developer);
+		System.out.println("isBeta "+isBeta);
+		System.out.println("The release year "+releaseYear);
+		System.out.println("The shortcutKey is "+shortcutKey);
+
+	}
+
+}
